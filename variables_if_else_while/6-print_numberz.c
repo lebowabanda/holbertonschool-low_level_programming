247cb5c8702ef7a6b5f8e29ddc0701223b3e6328
lebowabanda <lebowabanda@gmail.com>
2026-06-01 11:37:13 +0200

    You are not allowed to use any variable of type char You can only use the putchar function (every other function (printf, puts, etc…) is forbidden)

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers of base 10
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+    int i;
+
+    for (i = 48; i <= 57; i++)
+    {
+        putchar(i);
+    }
+
+    putchar('\n');
+
+    return (0);
+}

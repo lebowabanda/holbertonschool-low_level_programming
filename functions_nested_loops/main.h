b149dbe112aa383ea51ebdb40c137b7d49b35085
lebowabanda <lebowabanda@gmail.com>
2026-06-01 11:58:13 +0200

    a program that prints _putchar, followed by a new line.
    The program should return 0

---
+++ functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar (char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int n);
+int _isalpha(int c);
+int print_sign(int c);
+int _abs(int c);
+int print_last_digit(int c);
+void jack_bauer(void);
+#endif
